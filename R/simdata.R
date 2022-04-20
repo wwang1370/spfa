89f9765c2b55be82ed7d404a5348181caf2a7ba8
wwang1370 <weimengbonnie@gmail.com>
2022-04-19 23:14:43 -0400

    add simdata

---
+++ R/simdata.R
@@ -0,0 +1,8 @@
+#' Simulated dataset 
+#' 
+#' 
+#' There are 16 columns with 1000 rows. The first 8 columns contain continuous item response time data and the last 8 columns with discrete item responses. 
+#' Among the discrete items, there are 4 dichotomous and 4 four-category ones.
+#'
+#' head(simdata)
+"simdata"
