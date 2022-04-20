deaaa89c4862a482dce23bb6ff5b411a82368ff7
wwang1370 <weimengbonnie@gmail.com>
2022-04-19 23:22:58 -0400

    delete data and add simdata

--- data-raw/DATASET.R
+++
@@ -1,8 +0,0 @@
-## code to prepare `DATASET` dataset goes here
-library(tidyverse)
-
-PISA15MATH <- read_csv("PISA_2015_M1_M2_complete.csv") %>% 
-        select(starts_with("CM") & ends_with("T"))
-usethis::use_data(PISA15MATH, overwrite = TRUE, internal = T)
-
-
